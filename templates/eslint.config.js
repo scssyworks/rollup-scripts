7fdeda1a5247c3bbaf6871283c06f0697f7925ee
Sachin Singh <contactsachinsingh@gmail.com>
2023-06-11 16:33:34 +0530

    Added typescript parser

--- templates/eslint.config.js
+++ templates/eslint.config.js
@@ -1,7 +1,3 @@
-const { check, resolvePath } = require('../utils');
-const babelConfig = require('../templates/babel.config');
-const { configTypes } = require('../constants');
-
 module.exports = async (args) => {
   const { typescript, react } = args;
   const eslintBaseRc = {
@@ -19,20 +15,16 @@ module.exports = async (args) => {
   };
 
   if (typescript || react) {
-    const babelFile = await check(configTypes.BABEL);
-    const babelrc = !!babelFile;
-    eslintBaseRc.parser = '@babel/eslint-parser';
-    Object.assign(eslintBaseRc.parserOptions, {
-      requireConfigFile: babelrc,
-      babelOptions: {
-        babelrc,
-        configFile: babelrc ? resolvePath(babelFile) : false,
-        ...(babelrc ? {} : babelConfig(args)),
-      },
-    });
-    eslintBaseRc.plugins = ['@babel'];
+    if (react) {
+      Object.assign(eslintBaseRc.parserOptions, {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      });
+    }
     if (typescript) {
-      eslintBaseRc.plugins.push('@typescript-eslint');
+      eslintBaseRc.parser = '@typescript-eslint/parser';
+      eslintBaseRc.plugins = ['@typescript-eslint'];
       eslintBaseRc.extends.push('plugin:@typescript-eslint/recommended');
     }
   }
