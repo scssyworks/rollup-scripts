b429514a16a9199a4d74dc3b5d806eac3e0338a3
Sachin Singh <contactsachinsingh@gmail.com>
2023-06-15 21:58:13 +0530

    Fixed a minor bug

--- utils/getPackage.js
+++ utils/getPackage.js
@@ -42,9 +42,15 @@ function deps(keys = ['dependencies', 'devDependencies']) {
 
 function jsxImportSource() {
   const modules = deps().filter((module) => JSX_MODULES.includes(module));
+  if (modules.length === 0) {
+    // No JSX library found
+    return '';
+  }
   if (modules.length === 1) {
+    // JSX library found
     return modules[0];
   }
+  // More than one JSX libraries found
   throw new Error(ERR_JSX_MODULE(modules));
 }
 
