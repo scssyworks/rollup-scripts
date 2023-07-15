f92057e812fcb9a34784d47280f7531694ecc97e
Sachin Singh <contactsachinsingh@gmail.com>
2023-07-15 23:38:56 +0530

    Added tests and modified launch.json to debug only current file

---
+++ utils/output.test.js
@@ -0,0 +1,48 @@
+const { CONFIG_FILE } = require('../constants');
+const { resolveOutputFields } = require('./output');
+const { resolvePath } = require('./resolvePath');
+
+jest.mock('./getResource', () => ({
+  ...jest.requireActual('./getResource'),
+  fromPackage: (key) =>
+    key === 'main'
+      ? 'dist/cjs/index.js'
+      : key === 'module'
+      ? 'dist/es/index.js'
+      : '',
+}));
+
+const mockRsConfig = jest.fn();
+jest.mock('./rs', () => ({
+  ...jest.requireActual('./rs'),
+  getRsConfig: () => mockRsConfig(),
+}));
+
+describe('output', () => {
+  beforeEach(() => {
+    mockRsConfig.mockClear();
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+  describe('resolveOutputFields', () => {
+    it('should return output fields', () => {
+      mockRsConfig.mockReturnValue({});
+      expect(resolveOutputFields({ configFile: CONFIG_FILE })).toEqual({
+        es: resolvePath('dist/es/index.js'),
+        cjs: resolvePath('dist/cjs/index.js'),
+      });
+    });
+    it('should return output fields for other formats', () => {
+      mockRsConfig.mockReturnValue({
+        outputFormats: ['es', 'cjs', 'umd', 'iife'],
+      });
+      expect(resolveOutputFields({ configFile: CONFIG_FILE })).toEqual({
+        es: resolvePath('dist/es/index.js'),
+        cjs: resolvePath('dist/cjs/index.js'),
+        umd: resolvePath('dist/umd/index.js'),
+        iife: resolvePath('dist/iife/index.js'),
+      });
+    });
+  });
+});
