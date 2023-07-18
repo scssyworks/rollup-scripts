c45dbcdbb66ffae64ec957dcbb72b4b3a406585d
Sachin Singh <contactsachinsingh@gmail.com>
2023-07-18 14:07:08 +0530

    Minor bug fixes

--- templates/swc.config.js
+++ templates/swc.config.js
@@ -1,26 +1,28 @@
 const { jsxImportSource } = require('../utils');
 
 module.exports = ({ typescript, react, preact }) => {
+  const isJSX = react || preact;
   return {
     $schema: 'https://json.schemastore.org/swcrc',
     jsc: {
       parser: {
-        jsx: react || preact,
         decorators: true,
-        privateMethod: true,
-        classPrivateProperty: true,
-        topLevelAwait: true,
         dynamicImport: true,
         ...(typescript
           ? {
               syntax: 'typescript',
-              tsx: react || preact,
+              ...(isJSX ? { tsx: true } : {}),
             }
-          : {}),
+          : {
+              ...(isJSX ? { jsx: true } : {}),
+              privateMethod: true,
+              classPrivateProperty: true,
+              topLevelAwait: true,
+            }),
       },
       transform: {
         decoratorVersion: '2022-03', // Should be 2023-05 but swc is lagging behind
-        ...(react || preact
+        ...(isJSX
           ? {
               react: {
                 runtime: 'automatic',
