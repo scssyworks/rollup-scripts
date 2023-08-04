1f00a54dc675554e2553c6cab7b72eb75421d2c6
Sachin Singh <contactsachinsingh@gmail.com>
2023-08-04 23:37:39 +0530

    Fixed a bug with linting not picking up rules from config file

--- bin/rollup-scripts/lint/index.js
+++ bin/rollup-scripts/lint/index.js
@@ -28,14 +28,15 @@ module.exports = async function lint(args) {
     if (swc) {
       logger.warn(ERR_SWC_ESLINT);
     }
+    const useEslintrc = Boolean(check(configTypes.ESLINT));
     let errorCount = 0;
     let warningCount = 0;
     let totalFiles = 0;
     const overrideConfig = await eslintConfig(finalArgs);
     const eslint = new ESLint({
-      useEslintrc: Boolean(check(configTypes.ESLINT)),
-      overrideConfig,
+      useEslintrc,
       fix,
+      ...(!useEslintrc ? { overrideConfig } : {}),
     });
     const results = await eslint.lintFiles(resolvePath(`${srcRoot}/**/*`));
     totalFiles = results.length;
