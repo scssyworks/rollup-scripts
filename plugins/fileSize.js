a33131ed981c11d3e994bda9e59923540568c1cc
Sachin Singh <contactsachinsingh@gmail.com>
2024-03-29 15:28:41 +0530

    HTML template in progress

--- plugins/fileSize.js
+++ plugins/fileSize.js
@@ -11,35 +11,31 @@ const {
 module.exports = {
   fileSize(args) {
     const logger = getLogger(args);
-    return !args.watch
-      ? [
-          {
-            name: 'fileSize',
-            writeBundle: {
-              async handler(opts, bundle) {
-                if (isString(opts?.file) && bundle) {
-                  const filePath = opts.file.substring(
-                    0,
-                    crossPath(opts.file).lastIndexOf('/')
-                  );
-                  const bundleNames = Object.keys(bundle);
-                  for (const name of bundleNames) {
-                    const { type: fileType, map: fileMap } = bundle[name];
-                    if (fileType === 'chunk') {
-                      const actualPath = path.join(filePath, name);
-                      const relativePath = path.relative(cwd(), actualPath);
-                      const fileSize = await calculateSize(actualPath);
-                      logger.success(
-                        `${PREFIX}${relativePath}${fileMap ? ' ⚡︎' : ''}`,
-                        fileSize
-                      );
-                    }
-                  }
-                }
-              },
-            },
-          },
-        ]
-      : [];
+    return {
+      name: 'fileSize',
+      writeBundle: {
+        async handler(opts, bundle) {
+          if (isString(opts?.file) && bundle) {
+            const filePath = opts.file.substring(
+              0,
+              crossPath(opts.file).lastIndexOf('/')
+            );
+            const bundleNames = Object.keys(bundle);
+            for (const name of bundleNames) {
+              const { type: fileType, map: fileMap } = bundle[name];
+              if (fileType === 'chunk') {
+                const actualPath = path.join(filePath, name);
+                const relativePath = path.relative(cwd(), actualPath);
+                const fileSize = await calculateSize(actualPath);
+                logger.success(
+                  `${PREFIX}${relativePath}${fileMap ? ' ⚡︎' : ''}`,
+                  fileSize
+                );
+              }
+            }
+          }
+        },
+      },
+    };
   },
 };
